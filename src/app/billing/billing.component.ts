e67421884817e3706c34c9c6251843a4282b1eb4
guid21 <uryuk.uz@gmail.com>
2021-06-05 19:40:14 +0200

    Completed task

--- src/app/billing/billing.component.ts
+++ src/app/billing/billing.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'es-billing',
     templateUrl: './billing.component.html',
-    styleUrls: ['./billing.component.scss']
+    styleUrls: ['./billing.component.scss'],
 })
 export class BillingComponent implements OnInit {
+    statusEvent = new CustomEvent('status');
+    status: boolean = false;
 
-    constructor() { }
+    constructor() {}
 
-    ngOnInit(): void {
+    ngOnInit(): void {}
+
+    async setStatus() {
+        this.status = !this.status;
+        this.statusEvent.initCustomEvent('status', true, false, this.status);
+        document.dispatchEvent(this.statusEvent);
     }
-
 }
